b1a1a5b9965abea894334157b62aca60b5841a20
Rajat Deshmukh <80767058+Silentkiller9860@users.noreply.github.com>
2021-08-19 19:35:56 +0530

    main file with streamlit

--- main.py
+++ main.py
@@ -64,4 +64,5 @@ if st.button('Predict Price'):
     query = np.array([company,lapi_type,ram,weight,touchscreen,ips,cpu,hdd,ssd,gpu,ppi,os])
 
     query = query.reshape(1,12)
-    st.title("The predicted price is : " + np.exp(pipe.predict(query)[0]))
+    p=float(np.exp(pipe.predict(query)[0]))
+    st.title("The predicted price is : " + "{:.2f}".format(p) + "/- Rs")
